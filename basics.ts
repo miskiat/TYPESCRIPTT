dcc17cc0503fa25d3dffc47cd90b5515d7769ccc
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-02-18 16:34:27 +0100

    loops

--- basics.ts
+++ basics.ts
@@ -19,4 +19,19 @@ const myPerson: Person = {
   first: "Miskiat",
   last: "Badmus",
 };
-myPerson.
+
+const ids: Record<number, string> = {
+  10: "a",
+  20: "b",
+};
+ids[30] = "c";
+
+if ((ids[30] = "D")) {
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+[1, 2, 3].forEach((v) => console.log(v));
+const out: number[] = [4, 5, 6].map((v) => v * 10);
