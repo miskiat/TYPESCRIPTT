a50dd29985d6ac4e17f2eff151375c9aa9f63d05
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-02-18 16:17:41 +0100

    interface

--- basics.ts
+++ basics.ts
@@ -4,3 +4,19 @@ let hasLoggedIn: boolean = true;
 userName += " Badmus";
 
 console.log(userName);
+
+let myNumber: number = 10;
+
+let myRegex: RegExp = /foo/;
+
+const names: string[] = userName.split(" ");
+const myValues: Array<number> = [2, 3, 4];
+interface Person {
+  first: string;
+  last: string;
+}
+const myPerson: Person = {
+  first: "Miskiat",
+  last: "Badmus",
+};
+myPerson.
