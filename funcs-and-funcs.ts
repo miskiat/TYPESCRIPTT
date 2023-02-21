8fb01b53ff483eb5d7124f11f1b10086ff79d4a2
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-02-21 23:09:47 +0100

    function types

--- funcs-and-funcs.ts
+++ funcs-and-funcs.ts
@@ -8,13 +8,27 @@ export function printToFile(text: string, callback: () => void): void {
 
 //Array Mutation function
 
+//function as types
+export type MutationFunction = (v: number) => number;
+
 export function arrayMutate(
   numbers: number[],
-  mutate: (v: number) => number
+  mutate: MutationFunction
 ): number[] {
   return numbers.map(mutate);
 }
+
+const myNewMutateFunc: MutationFunction = (v: number) => v * 100;
+
 console.log(arrayMutate([1, 20, 3], (v) => v * 10));
 
-//function as types
-type MutationFunction = (v: number) => number;
+//Returning function
+export type AdderFunction = (v: number) => number;
+
+export function createAdder(num: number): AdderFunction {
+  return (val: number) => num + val;
+}
+
+const addOne = createAdder(1);
+console.log(addOne(55));
+// (val: number) => number
