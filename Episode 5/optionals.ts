eee9d075fed0de2bc6c4e0bb49185c4689562750
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-02-25 00:18:25 +0100

    optional fields and callbacks

--- Episode 5/optionals.ts
+++ Episode 5/optionals.ts
@@ -1,7 +1,39 @@
 ///Optional parameters
 function printIngredient(quantity: string, ingredient: string, extra?: string) {
-  console.log("${quantity} ${ingredient}");
+  console.log(`${quantity} ${ingredient} ${extra ? `${extra}` : ""}`);
 }
 
 printIngredient("1C", "Flour");
 printIngredient("1C", "Sugar", "Flour");
+// /// You cant put another  required  after optional
+
+// function printIngredient(
+//   quantity: string,
+//   ingredient: string,
+//   extra?: string,
+//   required: string
+// );
+
+///Optional fields
+interface User {
+  id: string;
+  info?: {
+    email?: string;
+  };
+}
+function getEmail(user: User): string {
+  if (user.info) {
+    return user.info.email!;
+  }
+  return "";
+}
+function getEmailEasy(user: User): string {
+  return user?.info?.email ?? "";
+}
+
+///optional callbacks
+
+function addWithCallback(x: number, y: number, callback?: () => void) {
+  console.log([x, y]);
+  callback?.();
+}
