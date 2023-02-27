9d8dde61c431698de5142f61e48d503e472d9df7
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-02-27 23:07:06 +0100

    usestate

--- Episode 6/tuples.ts
+++ Episode 6/tuples.ts
@@ -6,4 +6,23 @@ function add3DCoordinate(
 ): ThreeDCoordinate {
   return [c1[0] + c2[0], c1[1] + c2[1], c1[2] + c2[2]];
 }
-console.log(add3DCoordinate([0, 0, 0], [10, 20, 30]));
+console.log(add3DCoordinate([0, 100, 0], [10, 20, 30]));
+
+///Tuples with Different types
+///Usestate tuples returns a state and a state setter
+
+function simpleStringState(
+  initial: string
+): [() => string, (v: string) => void] {
+  let str: string = initial;
+  return [
+    () => str,
+    (v: string) => {
+      str = v;
+    },
+  ];
+}
+const [str1getter, str1setter] = simpleStringState("hello");
+console.log(str1getter());
+str1setter("goodbye");
+console.log(str1getter());
