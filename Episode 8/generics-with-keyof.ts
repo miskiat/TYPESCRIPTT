55a4516302367cbf2b2b95fd4db9d05df937307c
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-03-07 02:11:13 +0100

    generics with keyof

--- Episode 8/generics-with-keyof.ts
+++ Episode 8/generics-with-keyof.ts
@@ -12,3 +12,30 @@ const dogs = [
 
 console.log(pluck(dogs, "age"));
 console.log(pluck(dogs, "name"));
+
+///Event Map
+interface BaseEvent {
+  time: number;
+  user: string;
+}
+interface EventMap {
+  addToCart: BaseEvent & { quantity: number; productID: string };
+  checkout: BaseEvent;
+}
+
+function sendEvent<Name extends keyof EventMap>(
+  name: string,
+  data: unknown
+): void {
+  console.log([name, data]);
+}
+sendEvent("addToCart", {
+  productID: "foo",
+  user: "baz",
+  quantity: 1,
+  time: 10,
+});
+sendEvent("checkout", {
+  time: 20,
+  user: "bob",
+});
