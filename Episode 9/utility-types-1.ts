9d91f851e1c9b1b8cdd2cbffc0aec1a220ac687e
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-03-08 22:17:03 +0100

    utility types

--- Episode 9/utility-types-1.ts
+++ Episode 9/utility-types-1.ts
@@ -1,1 +1,18 @@
-interface;
+interface MyUser {
+  name: string;
+  id: string;
+  email?: string;
+}
+
+interface MyUserOptionals {
+  name?: string;
+  id?: string;
+  email?: string;
+}
+
+const merge = (user: MyUser, overrides: MyUserOptionals): MyUser => {
+  return {
+    ...user,
+    ...overrides,
+  };
+};
